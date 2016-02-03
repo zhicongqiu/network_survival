b534ae2c175724faaf21a3783c3d374840938b6e
zhicong qiu <zzq101@psu.edu>
2016-02-03 08:10:03 -0500

    add backup protection sim...

--- backup_protection_sim.m
+++ backup_protection_sim.m
@@ -1,10 +1,11 @@
-function [METRICS] = backup_protection_sim(GRAPH,rep_link,rep_list,ADJ,...
-					   pairs,shortest_ps,backup_ps,...
-					   HEMP,NUM)
+function [PAIRS_METRICS BASELINE_METRICS] = ...
+	 backup_protection_sim(GRAPH,rep_link,rep_list,ADJ,...
+			       pairs,shortest_ps,backup_ps,...
+			       BASELINE,HEMP,NUM)
 
 %function [METRICS] = backup_protection_sim(GRAPH,rep_link,rep_list,ADJ,...
-%					   pairs,shortest_p,backup_p,...
-%					   HEMP,NUM)
+%					   pairs,shortest_ps,backup_ps,...
+%					   BASELINE,HEMP,NUM)
 %INPUT:
 %GRAPH - node x,y locations
 %rep_link - repeater links
@@ -13,36 +14,43 @@ function [METRICS] = backup_protection_sim(GRAPH,rep_link,rep_list,ADJ,...
 %pairs - pairs of interested node, i.e., nodes within std of HEMP
 %shortest_p - precomputed shortest paths for each pair
 %backup_p - precomputed backup paths for each pair
+%BASELINE - an struct array containing baseline of interest (e.g., 2nd
+%nd shortest ps, etc.)
 %HEMP - HEMP model (gaussian HEMP attack)
 %NUM - number of MC sampling to the HEMP attack
 %OUTPUT:
 %not decided?
   num_pairs = size(pairs,1);
-  PAIR_MATRICS = zeros(size(num_pairs,4);
+  PAIRS_METRICS = zeros(num_pairs,5);
+  for i=1:length(BASELINE)
+    BASELINE_METRICS(i).PAIRS_METRICS = zeros(num_pairs,5);
+  end
   for i=1:NUM
     survived_nodes_indicator = zeros(1,size(GRAPH,1));
     %getting survival and failed nodes
     [survive_nodes failed_nodes count_survived count_failed] = ...
     generate_failures(GRAPH,HEMP,'gaussian');
     fail_node_count(i) = count_failed;
-
+    
+%{
     %get failed repeaters index, and hence failed links
     [survived_repeaters failed_repeaters count_survived count_failed] = ...
     generate_failures(rep_list,HEMP,'gaussian');
     fail_repeater_count(i) = count_failed;
+%}
     %post attack graph
     %initialize GRAPH and ADJ post-attack
     ADJ_post = ADJ;
     ADJ_failed = ADJ;
-    GRAPH_post = GRAPH;
-    GRAPH_failed = GRAPH;
-    index4post = index4original;
+
+%{
     if size(failed_repeaters,1)>0
       for j=1:size(failed_repeaters,1) %link failures
 	ADJ_post(rep_link(failed_repeaters(j),1),rep_link(failed_repeaters(j),2)) = 0;
 	ADJ_post(rep_link(failed_repeaters(j),2),rep_link(failed_repeaters(j),1)) = 0;
       end
     end
+%}
     %node failure
     ADJ_post(failed_nodes,:) = 0;
     ADJ_post(:,failed_nodes) = 0;
@@ -53,31 +61,28 @@ function [METRICS] = backup_protection_sim(GRAPH,rep_link,rep_list,ADJ,...
       if survived_nodes_indicator(pairs(j,1))==1&&...
 	 survived_nodes_indicator(pairs(j,2))==1
 	PAIRS_METRICS(j,1) = PAIRS_METRICS(j,1)+1;
-	%check if the primary survived
-	temp = true
-	for k=1:length(shortest_ps(j).p)-1
-          if ADJ_post(shortest_ps(j).p(k),shortest_ps(j).p(k+1))==0
-	    %primary failed
-            PAIRS_METRICS(j,3) = PAIRS_METRICS(j,3)+1;
-	    temp = false;
-	    break;
-	  end
-	end
+	temp = calculate_backup_survive(ADJ_post,shortest_ps(j).p);
 	%primary survived
 	if temp==true
           PAIRS_METRICS(j,2) = PAIRS_METRICS(j,2)+1;
 	else
-	  temp2 = true;
-	  for k=1:length(backup_ps)
-	    if ADJ_post(backup_ps(j).p(k),backup_ps(j).p(k+1))==0
-	      PAIRS_METRICS(j,5) = PAIRS_METRICS(j,5)+1;
-	      temp2 = false;
-	      break;
-	    end
-	  end
-	  if temp2==true
+	  PAIRS_METRICS(j,3) = PAIRS_METRICS(j,3)+1;
+	  temp = calculate_backup_survive(ADJ_post,backup_ps(j).p);
+	  if temp==false
+	    PAIRS_METRICS(j,5) = PAIRS_METRICS(j,5)+1;
+	  else
 	    PAIRS_METRICS(j,4) = PAIRS_METRICS(j,4)+1;
 	  end
+	  for k=1:length(BASELINE)
+	      temp = calculate_backup_survive(ADJ_post,BASELINE(k).backup_ps(j).p);
+	      if temp==false
+		BASELINE_METRICS(k).PAIRS_METRICS(j,5) = ...
+		BASELINE_METRICS(k).PAIRS_METRICS(j,5)+1;
+	      else
+		BASELINE_METRICS(k).PAIRS_METRICS(j,4) = ...
+		BASELINE_METRICS(k).PAIRS_METRICS(j,4)+1;
+	      end
+	  end	    
 	end
       end
     end
--- get_node_failure_prob.m
+++ get_node_failure_prob.m
@@ -2,13 +2,12 @@ function [node_fail_prob] = get_node_failure_prob(core_list,HEMP)
 
 N = size(core_list,1);
 node_fail_prob = zeros(N,1);
-for i=1:size(N,1)
+for i=1:N
     [temp_dist temp_az] = ...
     lon_lat_dist(HEMP.ground_zero(1),HEMP.ground_zero(2),...
-		 node_list(i,1),node_list(i,2));
+		 core_list(i,1),core_list(i,2));
 
     %excluding too faraway node
-    if temp_dist<4*HEMP.std
       %p-value associated with x and y
       temp_prob = normcdf(temp_dist,0,HEMP.std);
       if temp_prob<0.5
@@ -17,5 +16,4 @@ for i=1:size(N,1)
 	temp_prob = 2*(1-temp_prob);
       end
       node_fail_prob(i) = temp_prob;
-    end
 end
