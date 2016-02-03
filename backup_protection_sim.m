64c0206e096a9206a6f60bcfabdd4d108899ce59
zhicong.qiu <zhichong.qiu@gmail.com>
2016-02-02 18:15:15 -0800

    ...

--- backup_protection_sim.m
+++ backup_protection_sim.m
@@ -1,5 +1,5 @@
 function [METRICS] = backup_protection_sim(GRAPH,rep_link,rep_list,ADJ,...
-					   pairs,shortest_p,backup_p,...
+					   pairs,shortest_ps,backup_ps,...
 					   HEMP,NUM)
 
 %function [METRICS] = backup_protection_sim(GRAPH,rep_link,rep_list,ADJ,...
@@ -17,4 +17,68 @@ function [METRICS] = backup_protection_sim(GRAPH,rep_link,rep_list,ADJ,...
 %NUM - number of MC sampling to the HEMP attack
 %OUTPUT:
 %not decided?
+  num_pairs = size(pairs,1);
+  PAIR_MATRICS = zeros(size(num_pairs,4);
+  for i=1:NUM
+    survived_nodes_indicator = zeros(1,size(GRAPH,1));
+    %getting survival and failed nodes
+    [survive_nodes failed_nodes count_survived count_failed] = ...
+    generate_failures(GRAPH,HEMP,'gaussian');
+    fail_node_count(i) = count_failed;
 
+    %get failed repeaters index, and hence failed links
+    [survived_repeaters failed_repeaters count_survived count_failed] = ...
+    generate_failures(rep_list,HEMP,'gaussian');
+    fail_repeater_count(i) = count_failed;
+    %post attack graph
+    %initialize GRAPH and ADJ post-attack
+    ADJ_post = ADJ;
+    ADJ_failed = ADJ;
+    GRAPH_post = GRAPH;
+    GRAPH_failed = GRAPH;
+    index4post = index4original;
+    if size(failed_repeaters,1)>0
+      for j=1:size(failed_repeaters,1) %link failures
+	ADJ_post(rep_link(failed_repeaters(j),1),rep_link(failed_repeaters(j),2)) = 0;
+	ADJ_post(rep_link(failed_repeaters(j),2),rep_link(failed_repeaters(j),1)) = 0;
+      end
+    end
+    %node failure
+    ADJ_post(failed_nodes,:) = 0;
+    ADJ_post(:,failed_nodes) = 0;
+
+    survived_nodes_indicator(survive_nodes) = 1;
+    %check which pairs have survived
+    for j=1:num_pairs
+      if survived_nodes_indicator(pairs(j,1))==1&&...
+	 survived_nodes_indicator(pairs(j,2))==1
+	PAIRS_METRICS(j,1) = PAIRS_METRICS(j,1)+1;
+	%check if the primary survived
+	temp = true
+	for k=1:length(shortest_ps(j).p)-1
+          if ADJ_post(shortest_ps(j).p(k),shortest_ps(j).p(k+1))==0
+	    %primary failed
+            PAIRS_METRICS(j,3) = PAIRS_METRICS(j,3)+1;
+	    temp = false;
+	    break;
+	  end
+	end
+	%primary survived
+	if temp==true
+          PAIRS_METRICS(j,2) = PAIRS_METRICS(j,2)+1;
+	else
+	  temp2 = true;
+	  for k=1:length(backup_ps)
+	    if ADJ_post(backup_ps(j).p(k),backup_ps(j).p(k+1))==0
+	      PAIRS_METRICS(j,5) = PAIRS_METRICS(j,5)+1;
+	      temp2 = false;
+	      break;
+	    end
+	  end
+	  if temp2==true
+	    PAIRS_METRICS(j,4) = PAIRS_METRICS(j,4)+1;
+	  end
+	end
+      end
+    end
+  end
